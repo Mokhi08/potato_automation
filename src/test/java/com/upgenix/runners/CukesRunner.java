92069a2ea4c2bfa36d40e96db2b6ae7338b96eda
Mokhigul <moxigul.sadullayeva@mail.ru>
2023-03-15 21:25:08 +0100

    project framework has been created

---
+++ src/test/java/com/upgenix/runners/CukesRunner.java
@@ -0,0 +1,28 @@
+package com.upgenix.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                 "pretty",
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        },
+        features = "src/test/resources/features",
+        glue = "com/upgenix/step_definitions",
+        dryRun = false,
+        tags = ""
+
+
+
+)
+
+public class CukesRunner {
+
+}
---
+++ src/test/java/com/upgenix/utilities/Driver.java
@@ -0,0 +1,56 @@
+package com.upgenix.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    private Driver(){}
+
+    //  private static WebDriver driver;
+
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+
+    public static WebDriver getDriver(){
+
+        if(driverPool.get()==null){
+            String browserType = ConfigurationReader.getProperty("browser");
+
+            switch (browserType){
+
+                case "chrome":
+
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--remote-allow-origins=*");
+
+                    WebDriverManager.chromedriver().setup();
+                    driverPool.set(new ChromeDriver(chromeOptions));
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS );
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS );
+                    break;
+
+            }
+        }
+
+        return driverPool.get();
+    }
+
+    public static void closeDriver(){
+        if(driverPool.get()!=null){
+            driverPool.get().quit();
+            driverPool.remove();
+        }
+    }
+}
